d70e48ce7dcaa6b2f7f13038dba9480502bf940e
wb-jgh649382 <wb-jgh649382@alibaba-inc.com>
2020-12-21 15:13:03 +0800

    优化

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import Schedule from "./pages/index/index.jsx";
 
-// export default Schedule;
-ReactDOM.render(<App />, document.getElementById("root"));
+export default Schedule;
+// ReactDOM.render(<App />, document.getElementById("root"));
